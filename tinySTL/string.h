1dcb22d9af15deb3174fbb6f781ea08a64ae6bb6
Sun Zhi <imagecser@gmail.com>
2018-02-05 15:16:19 +0800

    string fix reverse_iterator

--- tinySTL/string.h
+++ tinySTL/string.h
@@ -166,7 +166,7 @@ namespace sz {
 			void resize(size_t n, const char ch = '\000');
 			void reserve(size_t n);
 			void shrink_to_fit();
-			void swap(string& str)
+			void swap(string& str);
 			size_t copy(char *s, size_t len, size_t pos = 0) const
 			string substr(size_t pos = 0, size_t len = npos) const
 
@@ -176,6 +176,12 @@ namespace sz {
 			const_iterator end() const;
 			const_iterator cbegin() const;
 			const_iterator cend() const;
+			reverse_iterator rbegin();
+			reverse_iterator rend();
+			const_reverse_iterator rbegin() const;
+			const_reverse_iterator rend() const;
+			const_reverse_iterator crbegin() const;
+			const_reverse_iterator crend() const;
 			size_t size() const;
 			size_t length() const;
 			size_t capacity() const;
@@ -426,10 +432,16 @@ namespace sz {
 		reverse_iterator rend() {
 			return reverse_iterator(_begin);
 		}
-		const_reverse_iterator crbegin() {
+		const_reverse_iterator rbegin() const {
 			return const_reverse_iterator(_end);
 		}
-		const_reverse_iterator crend() {
+		const_reverse_iterator rend() const {
+			return const_reverse_iterator(_begin);
+		}
+		const_reverse_iterator crbegin() const {
+			return const_reverse_iterator(_end);
+		}
+		const_reverse_iterator crend() const {
 			return const_reverse_iterator(_begin);
 		}
 		size_t size() const {
